bb0080bb5b84545685943d1656d6e7965d319997
Chen Xi-yang <settinghead@gmail.com>
2018-10-17 01:15:59 -0400

    mobilenet init

--- src/tf-test.ts
+++ src/tf-test.ts
@@ -1,1 +1,12 @@
-import tfjs from "@tensorflow/tfjs";
+import * as tf from "@tensorflow/tfjs";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+
+mobilenetTest();
+
+async function mobilenetTest() {
+  const model = await mobilenet.load();
+  console.log(model);
+  const img = tf.ones([1, 224, 224, 3]);
+  const predictions = await model.classify(img);
+  console.log(predictions);
+}
